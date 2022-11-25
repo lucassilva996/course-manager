ac89916c66599dd801f59dd34d47713ab5913236
lucassilva996 <lucassilva996@outlook.com>
2022-11-25 00:24:34 -0300

    instalação do bootstrap e criação do  component course-list

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
-  styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[] = [];
 
-  ngOnInit() {
+  ngOnInit() : void {
+    this.courses = [
+    {
+      id: 1,
+      name: 'Angular Forms',
+      imageUrl: '',
+      price: 99.99,
+      code: 'XPS-8796',
+      duration: 120,
+      rating: 5.4,
+      releaseDate: '10/10/2022'
+    },
+    {
+      id: 2,
+      name: 'Angular HTTP',
+      imageUrl: '',
+      price: 45.99,
+      code: 'LKL-1094',
+      duration: 80,
+      rating: 4,
+      releaseDate: '06/01/2018'
+    },
+    ]
   }
 
 }
